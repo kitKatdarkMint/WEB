227ca36094967f877bfb70c35f1759b1f25f7a37
abracadamain <paulineigue@gmail.com>
2025-03-26 15:00:42 +0100

    icone et logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,82 +1,80 @@
 function Navbar() {
-    return (
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            cuisINSA
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  Accueil
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Suggestion du jour
-                </a>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Recettes par catégorie
-                </a>
-                <ul className="dropdown-menu">
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Entrée
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Plat
-                    </a>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Dessert
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              
-            </ul>
-            <form className="d-flex" role="search">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Rechercher par recette, par ingrédient "
-                aria-label="Search"
-              />
-              <button className="btn btn-outline-success" type="submit">
-                Rechercher
-              </button>
-            </form>
-          </div>
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          <img src="/src/assets/cuisinsa.png" alt="cuisINSA" width="200" />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Accueil
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Suggestion du jour
+              </a>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Recettes par catégorie
+              </a>
+              <ul className="dropdown-menu">
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Entrée
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Plat
+                  </a>
+                </li>
+                <li>
+                  <hr className="dropdown-divider" />
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Dessert
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+          <form className="d-flex" role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Rechercher par recette, par ingrédient "
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Rechercher
+            </button>
+          </form>
         </div>
-      </nav>
-    );
-  }
-  
-  export default Navbar;
-  
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
